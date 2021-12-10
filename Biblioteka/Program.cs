f5ccdf384114503aa96e667a600d4e4b083dd6b3
Andrey Poplavskii <andris838@gmail.com>
2021-12-10 18:56:45 +0300

    init Biblioteka

---
+++ Biblioteka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Biblioteka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
