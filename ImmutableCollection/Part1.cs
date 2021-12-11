0410aaeda855b1ade17b7a5c159850c4382428c7
Andrey Poplavskii <andris838@gmail.com>
2021-12-11 23:09:43 +0300

    Доделал 3 приложение

---
+++ ImmutableCollection/Part1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImmutableCollection
+{
+    public class Part1
+    {
+        public ImmutableList<string> Poem { get; set; }
+
+        string _part;
+        public Part1(string part)
+        {
+            _part = part;   
+            ImmutableList<string> poem = ImmutableList.Create<string>();         
+        }
+
+        public ImmutableList<string> AddPart(ImmutableList<string> previouslist)
+        {
+            return Poem = previouslist.Add(_part);
+                
+        }
+    }
+}
---
+++ ImmutableCollection/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace ImmutableCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var part1 = new Part1("Вот дом,\nКоторый построил Джек.");
+            var part2 = new Part1("А это пшеница,\nКоторая в темном чулане хранится\nВ доме,\nКоторый построил Джек.");
+
+            part1.AddPart(ImmutableList.Create<string>());
+            part2.AddPart(part1.Poem);
+
+            Console.WriteLine("Первый абзац:");
+            Console.WriteLine("");
+            foreach (var part in part1.Poem)
+            {
+                Console.WriteLine(part);
+            }
+            
+            Console.WriteLine("");
+
+            Console.WriteLine("Первый - второй абзац:");
+            Console.WriteLine("");
+            foreach (var part in part2.Poem)
+            {
+                Console.WriteLine(part);
+            }
+
+            Console.WriteLine("");
+            
+
+
+
+        }
+    }
+}
